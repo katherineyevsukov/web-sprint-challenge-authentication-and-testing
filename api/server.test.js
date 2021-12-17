5f22d0c640bdc1df52c98e085a8c1d65532034d0
Kat <kyevsukov@gmail.com>
2021-12-17 11:26:38 -0500

    sanity tests written and passing

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,21 @@
-// Write your tests here
+const request = require('supertest')
+const db = require('./../data/dbConfig')
+const server = require('./server')
+const jokes = require('./jokes/jokes-data')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
 test('sanity', () => {
   expect(true).not.toBe(false)
+})
+
+test('correct env?', () => {
+  expect(process.env.NODE_ENV).toBe('testing')
 })
