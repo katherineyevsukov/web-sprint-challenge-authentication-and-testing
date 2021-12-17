3def214f439748bb71d02289fb0009bb0e739870
Kat <kyevsukov@gmail.com>
2021-12-17 11:07:10 -0500

    restricted mw working

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,18 @@
+const { JWT_SECRET } = require("../../config");
+const jwt = require('jsonwebtoken')
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if (!token) {
+    return next({ status: 401, message: "token required" });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next({ status: 401, message: "token invalid" });
+    }
+    req.decodedJwt = decoded;
+    next();
+  });
   /*
     IMPLEMENT
 
